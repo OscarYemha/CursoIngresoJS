3d4a873d4f5cc2a84f992ad639d94ce7cb851ae5
OscarYemha <oscaryemha1990@gmail.com>
2020-07-31 02:21:14 -0300

    Ejercicios

--- 5-InstruccionWhile/jsIteracionesWhile-07.js
+++ 5-InstruccionWhile/jsIteracionesWhile-07.js
@@ -3,31 +3,38 @@ Al presionar el botón pedir  números  hasta que el USUARIO QUIERA
 e informar la suma acumulada y el promedio.
 */
 function mostrar(){
-	
+
 	let i = 0;
 	let contador = 0;
 	let respuesta = prompt("Quiere sumar números?");
 	respuesta = respuesta.toLowerCase();
 
-	while(respuesta==="si"){
-		respuesta = prompt("Ingrese un número");
-		respuesta = parseInt(respuesta);
-		i+=respuesta;
-		respuesta = prompt("Quiere agregar otro número?");
-		respuesta = respuesta.toLowerCase();
-		if(respuesta==="si"){
-			contador ++;
-		}
-	}
+	switch(respuesta){
+		case "si":
+			while(respuesta === "si"){
+				respuesta = prompt("Ingrese un número");
+				respuesta = parseInt(respuesta);
+				i += respuesta;
+				respuesta = prompt("Quiere agregar otro número?");
+				respuesta = respuesta.toLowerCase();
+				if(respuesta === "si"){
+					contador++;
+				}
+			}
 
-	document.getElementById("txtIdSuma").value = i;
+			document.getElementById("txtIdSuma").value = i;
 
-	if(contador===0){
-		document.getElementById("txtIdPromedio").value=i;
-	}else{
-		document.getElementById("txtIdPromedio").value=i/2;
+			if(contador === 0){
+				document.getElementById("txtIdPromedio").value = i;
+			}else{
+				document.getElementById("txtIdPromedio").value = i / 2;
+			}
+
+			alert("Programa finalizado");
+			break;
+
+		default:
+			alert("Programa finalizado");
+			break;
 	}
-	
-	alert("Programa finalizado");
-	
-}//FIN DE LA FUNCIÓN
+}
