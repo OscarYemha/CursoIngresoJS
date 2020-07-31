31a6f5f8a1e8f41fb291c0869ed0b4d487f4bb4a
OscarYemha <oscaryemha1990@gmail.com>
2020-07-30 21:55:00 -0300

    Ejercicios

--- 5-InstruccionWhile/jsIteracionesWhile-03.js
+++ 5-InstruccionWhile/jsIteracionesWhile-03.js
@@ -2,7 +2,7 @@
 al presionar el botón pedir la CLAVE (ayuda: es utn750)*/
 function mostrar()
 {
-	var claveIngresada;
-	claveIngresada = prompt("ingrese el número clave.");
-	
+	let claveIngresada = prompt("ingrese el número clave.");
+	let clave = "utn750";
+	 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteracionesWhile-04.js
+++ 5-InstruccionWhile/jsIteracionesWhile-04.js
@@ -3,7 +3,9 @@ al presionar el botón
 pedir un número entre 0 y 9 inclusive.*/
 function mostrar()
 {
-	var numeroIngresado;
-	numeroIngresado = prompt("ingrese un número entre 0 y 10.");
+	let numeroIngresado = prompt("ingrese un número entre 0 y 10.");
 	
+	if(numeroIngresado<0||numeroIngresado>10){
+		numeroIngresado;
+	}
 }//FIN DE LA FUNCIÓN
